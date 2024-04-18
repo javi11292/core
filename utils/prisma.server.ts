0882635496e77064706c07ecd2366dd6f8b925a6
Javier Robles <javiroblesfernandez@gmail.com>
2024-04-18 19:43:43 +0200

    Add prisma

---
+++ utils/prisma.server.ts
@@ -0,0 +1,13 @@
+import { PrismaD1 } from "@prisma/adapter-d1";
+import { PrismaClient } from "@prisma/client";
+
+let prismaClient: PrismaClient;
+
+export const prisma = (platform: App.Platform) => {
+	if (!prismaClient) {
+		const adapter = new PrismaD1(platform.env.DB);
+		prismaClient = new PrismaClient({ adapter });
+	}
+
+	return prismaClient;
+};
