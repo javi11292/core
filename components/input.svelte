52e1a5d425052e43a47faf2093992be93288194f
Javier Robles <javiroblesfernandez@gmail.com>
2024-04-18 21:42:26 +0200

    Refactor

---
+++ components/input.svelte
@@ -0,0 +1,165 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+	import type {
+		FormEventHandler,
+		HTMLInputAttributes,
+		HTMLTextareaAttributes,
+	} from "svelte/elements";
+
+	type Props = {
+		regex?: RegExp;
+		icon?: Snippet;
+		label?: string;
+		value?: unknown;
+		disableShrink?: boolean;
+		disableFocusLabel?: boolean;
+		disabled?: boolean;
+	};
+
+	type InputProps = Props & { rows?: undefined } & HTMLInputAttributes;
+	type TextareaProps = Props & { rows: number } & HTMLTextareaAttributes;
+
+	let {
+		// eslint-disable-next-line no-undef
+		value = $bindable(),
+		regex,
+		label,
+		disableShrink,
+		disableFocusLabel,
+		disabled,
+		icon,
+		...props
+	}: InputProps | TextareaProps = $props();
+
+	const handleInput: FormEventHandler<HTMLInputElement | HTMLTextAreaElement> = ({
+		currentTarget,
+	}) => {
+		if (!regex || regex.test(currentTarget.value)) {
+			value = currentTarget.value;
+		} else {
+			currentTarget.value = value;
+		}
+	};
+</script>
+
+<div class:disabled class:focus-label={!disableFocusLabel} class="input">
+	{#if label}
+		<div class="label-space"></div>
+	{/if}
+
+	<div class:with-icon={icon} class="label-container">
+		{#if label}
+			<div class:disable-shrink={value || disableShrink} class="label">
+				{label}
+			</div>
+		{/if}
+	</div>
+
+	{#if props.rows !== undefined}
+		<textarea
+			{...props}
+			class:disabled
+			class:with-icon={icon}
+			oninput={handleInput}
+			aria-label={label}
+			{value}
+			{disabled}
+		></textarea>
+	{:else}
+		<input
+			{...props}
+			class:disabled
+			class:with-icon={icon}
+			oninput={handleInput}
+			aria-label={label}
+			{value}
+			{disabled}
+		/>
+	{/if}
+
+	{#if icon}
+		<div class="icon">
+			{@render icon()}
+		</div>
+	{/if}
+</div>
+
+<style lang="scss">
+	input,
+	textarea {
+		all: unset;
+		grid-column-start: 1;
+		grid-row-start: 2;
+		padding: 0.25rem 0;
+		box-sizing: border-box;
+		width: 100%;
+		transition: all;
+		transition-duration: 200ms;
+	}
+
+	.input {
+		display: grid;
+		border-bottom: 1px solid;
+		transition: all 200ms;
+		border-bottom: 1px solid currentColor;
+		position: relative;
+
+		&:focus-within {
+			border-color: var(--focus-color);
+		}
+	}
+
+	.icon {
+		grid-column-start: 1;
+		grid-row-start: 2;
+		margin-left: auto;
+		align-self: center;
+	}
+
+	.with-icon {
+		padding-right: 1.5rem;
+	}
+
+	.label-space {
+		height: 1rem;
+		grid-column-start: 1;
+		grid-row-start: 1;
+	}
+
+	.label-container {
+		pointer-events: none;
+		position: relative;
+		grid-column-start: 1;
+		grid-row-start: 1;
+		grid-row-end: 3;
+		overflow: hidden;
+	}
+
+	.focus-label {
+		&:focus-within {
+			.label {
+				@extend .disable-shrink;
+				color: var(--focus-color);
+			}
+		}
+	}
+
+	.label {
+		position: absolute;
+		top: 1rem;
+		transform-origin: left;
+		white-space: nowrap;
+		transition: all;
+		transition-duration: 200ms;
+	}
+
+	.disable-shrink {
+		translate: 0 -100%;
+		font-size: 0.75rem;
+		line-height: 1rem;
+	}
+
+	.disabled {
+		color: var(--disabled-color);
+	}
+</style>
--- utils/prisma.ts
+++ utils/prisma.ts
@@ -3,9 +3,16 @@ import { PrismaClient } from "@prisma/client";
 
 let prismaClient: PrismaClient;
 
-export const prisma = (platform: App.Platform) => {
+export const prisma = (
+	platform: App.Platform | undefined,
+	key: keyof App.Platform["env"] = "DB",
+) => {
+	if (!platform) {
+		return prismaClient;
+	}
+
 	if (!prismaClient) {
-		const adapter = new PrismaD1(platform.env.DB);
+		const adapter = new PrismaD1(platform.env[key]);
 		prismaClient = new PrismaClient({ adapter });
 	}
 
