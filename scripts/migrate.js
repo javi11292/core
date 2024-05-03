773bea3ae5f351e1e48a701c1e821df61cc2c02a
Javier Robles <javiroblesfernandez@gmail.com>
2024-05-04 00:38:34 +0200

    Add migrate script

---
+++ scripts/migrate.js
@@ -0,0 +1,28 @@
+import { spawnSync } from "node:child_process";
+
+const database = process.argv[2];
+const message = process.argv[3];
+
+const CREATE = `wrangler d1 migrations create ${database} ${message}`;
+const APPLY = `wrangler d1 migrations apply ${database}`;
+const DIFF =
+	"prisma migrate diff --from-local-d1 --to-schema-datamodel ./prisma/schema.prisma --script --output";
+
+const run = (/** @type {string} */ command) => {
+	console.log(`> ${command}\n`);
+
+	const result = spawnSync(command, {
+		stdio: ["inherit", "pipe", "inherit"],
+		shell: true,
+	})
+		.stdout.toString()
+		.trim();
+
+	result && console.log(`${result}\n`);
+
+	return result;
+};
+
+const path = run(CREATE).split(/\n/).at(-1);
+run(`${DIFF} ${path}`);
+run(APPLY);
