cc06443ca7fe49ae6ac8ff2f766a7d12f59642cb
Javier Robles <javiroblesfernandez@gmail.com>
2024-05-04 14:13:45 +0200

    Improve migrate

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -2,6 +2,7 @@ import { spawnSync } from "node:child_process";
 
 const database = process.argv[2];
 const message = process.argv[3];
+const remote = process.argv[4];
 
 const CREATE = `wrangler d1 migrations create ${database} ${message}`;
 const APPLY = `wrangler d1 migrations apply ${database}`;
@@ -23,6 +24,9 @@ const run = (/** @type {string} */ command) => {
 	return result;
 };
 
-const path = run(CREATE).split(/\n/).at(-1);
-run(`${DIFF} ${path}`);
-run(APPLY);
+if (message) {
+	const path = run(CREATE).split(/\n/).at(-1);
+	run(`${DIFF} ${path}`);
+}
+
+run(remote ? `${APPLY} ${remote}` : APPLY);
