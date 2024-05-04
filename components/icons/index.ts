6e3ea71f8120c94f143a1836824c3d1b109c425b
Javier Robles <javiroblesfernandez@gmail.com>
2024-05-04 12:29:29 +0200

    Refactor icons

---
+++ components/icons/index.ts
@@ -0,0 +1,6 @@
+export { default as add } from "./add.svg";
+export { default as arrowDown } from "./arrow-down.svg";
+export { default as arrowRight } from "./arrow-right.svg";
+export { default as cart } from "./cart.svg";
+export { default as delete } from "./delete.svg";
+export { default as remove } from "./remove.svg";
--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,6 @@
 export { default as Button } from "./button/button.svelte";
 export { default as Icon } from "./icon/icon.svelte";
+export * as icons from "./icons";
 export { default as Input } from "./input/input.svelte";
 export { default as Layout } from "./layout/layout.svelte";
 export { default as LoadingIcon } from "./loading-icon/loading-icon.svelte";
