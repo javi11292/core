a361ecdb09654a5e62310d830344b2f56c05638a
Javier Robles <javiroblesfernandez@gmail.com>
2024-06-11 12:53:25 +0200

    Remove jwt

--- utils/session.server.ts
+++ utils/session.server.ts
@@ -1,14 +1,39 @@
 import { prisma } from "$lib/core/utils/prisma.server";
 import { error, type Cookies } from "@sveltejs/kit";
-import jwt, { type JwtPayload } from "jsonwebtoken";
+import { createHmac, randomUUID } from "node:crypto";
 
-const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || "";
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "";
 const REFRESH_TOKEN = "refreshToken";
 const ACCESS_TOKEN = "accessToken";
 const ONE_YEAR = 31536000;
 
-const refreshAccessToken = async (cookies: Cookies, platform: App.Platform | undefined) => {
+type Event = {
+	cookies: Cookies;
+	platform: App.Platform | undefined;
+};
+
+const generateAccessToken = (session: string) => {
+	const data = Buffer.from(JSON.stringify([session, Math.floor(Date.now() / 1000) + 10])).toString(
+		"base64",
+	);
+	const signature = createHmac("sha256", ACCESS_TOKEN_SECRET).update(data).digest("base64");
+
+	return `${data}.${signature}`;
+};
+
+const verifyAccessToken = (token: string) => {
+	const [data, signature] = token.split(".");
+
+	if (createHmac("sha256", ACCESS_TOKEN_SECRET).update(data).digest("base64") !== signature) {
+		throw new Error();
+	}
+
+	const [session, expires] = JSON.parse(Buffer.from(data, "base64").toString());
+
+	return { session: session as string, expired: expires < Date.now() / 1000 };
+};
+
+const refreshAccessToken = async (session: string, { cookies, platform }: Event) => {
 	const refreshToken = cookies.get(REFRESH_TOKEN);
 	const accessToken = cookies.get(ACCESS_TOKEN);
 
@@ -16,47 +41,28 @@ const refreshAccessToken = async (cookies: Cookies, platform: App.Platform | und
 		throw new Error();
 	}
 
-	const { id } = jwt.verify(refreshToken, REFRESH_TOKEN_SECRET) as JwtPayload;
-	const accessPayload = jwt.verify(accessToken, ACCESS_TOKEN_SECRET, {
-		ignoreExpiration: true,
-	}) as JwtPayload;
-
-	if (id !== accessPayload.id) {
+	if (refreshToken !== session) {
 		throw new Error();
 	}
 
 	const user = await prisma(platform).user.findUnique({
-		where: { id },
+		where: { session },
 	});
 
-	if (!user || user.session !== refreshToken) {
+	if (!user) {
 		throw new Error();
 	}
 
-	cookies.set(ACCESS_TOKEN, jwt.sign({ id }, ACCESS_TOKEN_SECRET, { expiresIn: "5m" }), {
+	cookies.set(ACCESS_TOKEN, generateAccessToken(session), {
 		path: "/",
 		sameSite: "strict",
 		maxAge: ONE_YEAR,
 	});
 
-	return user.id;
+	return session;
 };
 
-const handleError = async (cookies: Cookies, platform?: App.Platform) => {
-	try {
-		return await refreshAccessToken(cookies, platform);
-	} catch {
-		error(401);
-	}
-};
-
-export const verifyAccessToken = async ({
-	cookies,
-	platform,
-}: {
-	cookies: Cookies;
-	platform: App.Platform | undefined;
-}) => {
+export const getSession = async ({ cookies, platform }: Event) => {
 	const accessToken = cookies.get(ACCESS_TOKEN);
 
 	if (!accessToken) {
@@ -64,19 +70,16 @@ export const verifyAccessToken = async ({
 	}
 
 	try {
-		const { id } = jwt.verify(accessToken, ACCESS_TOKEN_SECRET) as JwtPayload;
-		return id as number;
+		const { session, expired } = verifyAccessToken(accessToken);
+		return expired ? await refreshAccessToken(session, { cookies, platform }) : session;
 	} catch {
-		return handleError(cookies, platform);
+		error(401);
 	}
 };
 
-export const setRefreshToken = async (
-	id: number,
-	{ cookies, platform }: { cookies: Cookies; platform: App.Platform | undefined },
-) => {
-	const refreshToken = jwt.sign({ id }, REFRESH_TOKEN_SECRET, { expiresIn: "1y" });
-	const accessToken = jwt.sign({ id }, ACCESS_TOKEN_SECRET, { expiresIn: "5m" });
+export const setSession = async (id: number, { cookies, platform }: Event) => {
+	const refreshToken = randomUUID();
+	const accessToken = generateAccessToken(refreshToken);
 
 	cookies.set(REFRESH_TOKEN, refreshToken, { path: "/", sameSite: "strict", maxAge: ONE_YEAR });
 	cookies.set(ACCESS_TOKEN, accessToken, { path: "/", sameSite: "strict", maxAge: ONE_YEAR });
