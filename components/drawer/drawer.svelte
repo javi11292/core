2e0438c7a6333ffa3e729324952c4a176f509dd4
Javier Robles <javiroblesfernandez@gmail.com>
2024-06-16 18:13:34 +0200

    Add drawer

--- components/drawer/drawer.svelte
+++ components/drawer/drawer.svelte
@@ -1,14 +1,34 @@
 <script lang="ts">
-	let { open }: { open: boolean } = $props();
+	import type { Snippet } from "svelte";
+	import { fade } from "svelte/transition";
+
+	let { open = $bindable(), children }: { open: boolean; children: Snippet } = $props();
 </script>
 
-<section class:open>HOLA CARACOLA</section>
+{#if open}
+	<div
+		role="presentation"
+		onclick={() => (open = false)}
+		class="backdrop"
+		transition:fade={{ duration: 200 }}
+	></div>
+{/if}
+<section class:open>
+	{@render children()}
+</section>
 
 <style lang="scss">
+	.backdrop {
+		position: fixed;
+		inset: 0;
+		background: rgba(0, 0, 0, 0.8);
+	}
+
 	section {
 		background: white;
 		position: fixed;
 		left: 0;
+		top: 0;
 		height: 100%;
 		transition: translate 200ms;
 
