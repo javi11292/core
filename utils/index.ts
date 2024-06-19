a1067d668b3b02faf47b4ab15964dc4bac057ff9
Javier Robles <javiroblesfernandez@gmail.com>
2024-06-19 12:05:43 +0200

    Add classes

--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,19 @@
 export * from "./fetch";
 export * from "./transition";
+
+export const classes = (...names: (string | undefined | null | false)[]) => {
+	let className = "";
+
+	for (const name of names) {
+		if (!name) continue;
+
+		if (!className) {
+			className = name;
+			continue;
+		}
+
+		className = `${className} ${name}`;
+	}
+
+	return className;
+};
