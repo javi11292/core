072e31c03b955c9bd895953a16259d0eb76088c3
Javier Robles <javiroblesfernandez@gmail.com>
2024-06-20 13:16:47 +0200

    Remove classes util

--- utils/index.ts
+++ utils/index.ts
@@ -1,23 +1,6 @@
 export * from "./fetch";
 export * from "./transition";
 
-export const classes = (...names: (string | undefined | null | false)[]) => {
-	let className = "";
-
-	for (const name of names) {
-		if (!name) continue;
-
-		if (!className) {
-			className = name;
-			continue;
-		}
-
-		className = `${className} ${name}`;
-	}
-
-	return className;
-};
-
 export const dynamic = (callback: () => Promise<{ default: unknown }>) => async () => {
 	const component = await callback();
 
