f008b20ae80b7d945cb32dbbcf2622709297cd10
Javier Robles <javiroblesfernandez@gmail.com>
2024-06-21 14:00:43 +0200

    Test

--- components/layout/layout.svelte
+++ components/layout/layout.svelte
@@ -11,7 +11,7 @@
 	$effect(() => {
 		if (!dev && "serviceWorker" in navigator) {
 			navigator.serviceWorker.register("/service-worker.js").then((worker) => {
-				console.log("WORKER");
+				console.log("WORKER 2");
 				if (worker.active || !navigator.serviceWorker.controller) {
 					console.log("ACTIVE");
 					return;
