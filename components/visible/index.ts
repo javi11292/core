9609d2518d9b405b86bf6d3776c1f4150f9c9103
Javier Robles <javiroblesfernandez@gmail.com>
2024-06-26 18:07:16 +0200

    Add visible component

---
+++ components/visible/index.ts
@@ -0,0 +1,1 @@
+export { default as Visible } from "./visible.svelte";
---
+++ components/visible/visible.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+
+	type Props = { children: Snippet } & ({ web?: false; app: true } | { web: true; app?: false });
+
+	let { children, web, app }: Props = $props();
+</script>
+
+<div class:web class:app>
+	{@render children()}
+</div>
+
+<style>
+	@import "./visible.scss";
+</style>
