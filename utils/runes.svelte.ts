866507ac769d235410459b30567ba9671f97e124
Javier Robles <javiroblesfernandez@gmail.com>
2024-07-26 16:48:32 +0200

    Improve state rune

--- utils/runes.svelte.ts
+++ utils/runes.svelte.ts
@@ -1,9 +1,15 @@
 export class State<T = undefined> {
 	value: T = $state() as T;
+	private initialValue?: () => T;
 
-	constructor();
-	constructor(value: T);
-	constructor(value?: T) {
+	constructor(value?: T, initialValue?: () => T) {
 		this.value = value as T;
+		this.initialValue = initialValue;
+	}
+
+	reset() {
+		if (this.initialValue) {
+			this.value = this.initialValue();
+		}
 	}
 }
