acd242e7dc1748dcf42d31ceed80e51f5950d170
javi11292 <javi_1_12_92@hotmail.com>
2024-07-28 16:28:25 +0200

    Refactor

--- utils/runes.svelte.ts
+++ utils/runes.svelte.ts
@@ -1,15 +1,42 @@
 export class State<T = undefined> {
 	state: T = $state() as T;
-	private initialValue?: () => T;
+	private initial?: () => T;
 
-	constructor(value?: T, initialValue?: () => T) {
+	static create<T = undefined>(value?: T, initial?: () => T) {
+		let store: State<T>;
+
+		return {
+			create: () => {
+				store = new State(value, initial);
+			},
+
+			get state() {
+				return store.state;
+			},
+
+			set state(value: T) {
+				store.state = value;
+			},
+
+			get reset() {
+				return () => store.reset();
+			},
+		};
+	}
+
+	constructor(value?: T, initial?: () => T) {
 		this.state = value as T;
-		this.initialValue = initialValue;
+		this.initial = initial;
 	}
 
 	reset() {
-		if (this.initialValue) {
-			this.state = this.initialValue();
+		if (this.initial) {
+			this.state = this.initial();
 		}
 	}
 }
+
+export const effect = (callback: Parameters<typeof $effect>[0]) =>
+	$effect.root(() => {
+		$effect(callback);
+	});
