553c0c4d44ddd46fc8494410a4ba87761d9c811c
javi11292 <javi_1_12_92@hotmail.com>
2024-07-28 23:54:56 +0200

    Remove create

--- utils/runes.svelte.ts
+++ utils/runes.svelte.ts
@@ -2,28 +2,6 @@ export class State<T = undefined> {
 	state: T = $state() as T;
 	private initial?: () => T;
 
-	static create<T = undefined>(value?: T, initial?: () => T) {
-		let store: State<T>;
-
-		return {
-			create: () => {
-				store = new State(value, initial);
-			},
-
-			get state() {
-				return store.state;
-			},
-
-			set state(value: T) {
-				store.state = value;
-			},
-
-			get reset() {
-				return () => store.reset();
-			},
-		};
-	}
-
 	constructor(value?: T, initial?: () => T) {
 		this.state = value as T;
 		this.initial = initial;
