fd4d1f61e31c3294189209534f7488b7e2d81cb0
Javier Robles <javiroblesfernandez@gmail.com>
2024-10-03 23:10:50 +0200

    Add translations util

---
+++ utils/translate.ts
@@ -0,0 +1,21 @@
+import { State } from "./runes.svelte";
+
+const translations = new State<Record<string, string>>();
+
+export const translate = (key: string) => translations.state[key];
+
+export const loadTranslations = async (locales: string[]) => {
+	let locale: string | undefined;
+
+	for (let i = 0; i < navigator.languages.length; i++) {
+		locale = navigator.languages[i]?.match(/([^-]+)/)?.[1];
+
+		if (locale && locales.includes(locale)) {
+			break;
+		}
+	}
+
+	translations.state = await fetch(`/locales/${locale ?? locales[0]}.json`).then((response) =>
+		response.json(),
+	);
+};
