a2c259fa38d27d2690f93dc2fc03a6c56ef9d86f
Javier Robles <javiroblesfernandez@gmail.com>
2024-11-01 14:38:56 +0100

    Refactor

--- utils/supabase.server.ts
+++ utils/supabase.server.ts
@@ -1,4 +1,5 @@
-import { SUPABASE_KEY, SUPABASE_URL } from "$env/static/private";
+import { SUPABASE_KEY } from "$env/static/private";
+import { PUBLIC_SUPABASE_URL } from "$env/static/public";
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(PUBLIC_SUPABASE_URL, SUPABASE_KEY);
