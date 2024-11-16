b4b866c1f6fcec25755d9fac579fa0e475992d5c
Javier Robles <javiroblesfernandez@gmail.com>
2024-11-16 13:43:29 +0100

    Add confirmation

---
+++ components/confirmation/confirmation.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+	import { Modal } from "../modal";
+
+	type Props = {
+		children: Snippet;
+		buttons: Snippet<[() => void]>;
+		onclose: () => void;
+	};
+
+	let { children, buttons, onclose }: Props = $props();
+	let open = $state(true);
+</script>
+
+<Modal {open} {onclose}>
+	<div class="container">
+		{@render children()}
+
+		<div class="buttons">
+			{@render buttons(() => (open = false))}
+		</div>
+	</div>
+</Modal>
+
+<style>
+	@import "./confirmation.scss";
+</style>
